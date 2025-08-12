b303254075e160a916277e1bc5984800e289e8e8
Carlos Navajas <c.navajas4@gmail.com>
2025-08-12 11:44:41 +0200

    experiment with IEEE-754 mantissa calculation using bit ops

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,21 @@
 fn main() {
     let n: f32 = 12.12;
     let n_bits = n.to_bits();
-    let exponent = n_bits >> 23;
-    println!("{:032b}", exponent);
-    let mask = 0xff;
-    println!("{:032b}", mask);
-    let exp_mask = exponent & mask;
-    println!("{:032b}", exp_mask);
+    let mut mantissa: f32 = 1.0;
+
+    for i in 0..23 {
+        //println!("\n");
+        let mask = 1 << i;
+        let one_at_bit_i = n_bits & mask;
+        //if one_at_bit_i != 0 {
+        // if (n_bits  & (1 << i)) != 0 {
+        if ((n_bits >> i) & 1) == 1 {
+            let i_ = i as f32;
+            let weight = 2_f32.powf(i_ - 23.0);
+            mantissa += weight;
+            println!("{:>2} - {:032b} - {:032b}", i, mantissa.to_bits(), weight.to_bits());
+        }
+    }
+
+    //println!("{}", mantissa);
 }
